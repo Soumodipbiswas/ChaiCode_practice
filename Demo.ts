f48963f878f5d227f230d633951aea29f226d045
SOUMODIP BISWAS <soumodipbiswas80@gmail.com>
2026-03-10 21:43:25 +0530

    first commit

---
+++ Demo.ts
@@ -0,0 +1,13 @@
+function add1(a: number, b: number) {
+    return a + b;
+}
+
+const result1 = add1(5, 7);
+console.log(result1);
+
+function add2(a: number, b: number): string {
+    return a.toString() + b.toString();
+}
+
+const result2 = add2(5, 7);
+console.log(result2);
