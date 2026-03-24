e64530920ca9e09d36853b48779992ac3011ff7a
SOUMODIP BISWAS <soumodipbiswas80@gmail.com>
2026-03-24 13:17:12 +0530

    eighth commit

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,14 +1,21 @@
 import express from 'express';
 import type { Application } from "express";
+import todoRouter from "./todo/routes.js";
 
 export function createServerApp(): Application {
     const app = express();
 
-    app.get('/', function (req, res) {
-        return res.json({message: 'Hello world this is Soumodip'})
-    })
-    app.get('/hello', function (req, res) {
-        return res.json({message: 'Bye world'})
-    })
+    // //#region //*========== Routes ================
+    // app.get('/', function (req, res) {
+    //     return res.json({message: 'Hello world this is Soumodip'})
+    // })
+    // app.get('/hello', function (req, res) {
+    //     return res.json({message: 'Bye world'})
+    // })
+    // //#endregion //*========== Routes ================
+
+    app.use('/todos', todoRouter)
+    
+    
     return app;
 }
